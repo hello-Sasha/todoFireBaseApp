d6af201441afb04e011a2e8a4f62068ab58cbe65
Aleksandra V <vab.work@gmail.com>
2022-03-30 09:50:51 -0700

    Updates

---
+++ precache-manifest.2d911dbc0506206b8ea286100eab2baa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "054d5f3677d271308a0e",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/js/main.054d5f36.chunk.js"
+  },
+  {
+    "revision": "6ab03385acc36f76f69c",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/css/1.2b8ebb6a.chunk.css"
+  },
+  {
+    "revision": "6ab03385acc36f76f69c",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/js/1.6ab03385.chunk.js"
+  },
+  {
+    "revision": "38a6318109c6d71f3bdb",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/js/runtime~main.38a63181.js"
+  },
+  {
+    "revision": "4a2f6d1316cc560ede07d3334d3b486a",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-cyrillic-300-normal.4a2f6d13.woff2"
+  },
+  {
+    "revision": "853ac2aeeb42298d3b87f80fdf1f9486",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-greek-ext-300-normal.853ac2ae.woff2"
+  },
+  {
+    "revision": "607808ee335a962bdfa989bbfd5e8c57",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-cyrillic-ext-300-normal.607808ee.woff2"
+  },
+  {
+    "revision": "130eafc23a987a6cf560c9b69af84818",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-all-300-normal.130eafc2.woff"
+  },
+  {
+    "revision": "d47048a60dc372aa9776bb0b816bb238",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-vietnamese-300-normal.d47048a6.woff2"
+  },
+  {
+    "revision": "203e97b336d3bc83c8b492a9868d5574",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-greek-300-normal.203e97b3.woff2"
+  },
+  {
+    "revision": "065438c98de2b7f979decf5d7e3eb8a0",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-latin-ext-300-normal.065438c9.woff2"
+  },
+  {
+    "revision": "80fe119e5efa3911b9d61b265f723b3d",
+    "url": "/hello-Sasha/todoFireBaseApp.git/static/media/roboto-latin-300-normal.80fe119e.woff2"
+  },
+  {
+    "revision": "3716d623af5503170dca209cd352ff41",
+    "url": "/hello-Sasha/todoFireBaseApp.git/index.html"
+  }
+];
